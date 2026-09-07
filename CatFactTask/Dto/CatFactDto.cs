9242f7c05c98c92b1d48451cfc726dd1e47450cb
Jakub Tomaszewski <jakubtomaszewski@vp.pl>
2026-09-07 21:25:30 +0200

    add CatFact dto

---
+++ CatFactTask/Dto/CatFactDto.cs
@@ -0,0 +1,11 @@
+﻿using System.Text.Json.Serialization;
+
+namespace CatFactTask.Dto;
+
+public class CatFactDto
+{
+    [JsonPropertyName("fact")]
+    public string Fact { get; init; } = string.Empty;
+    [JsonPropertyName("length")]
+    public int Length { get; init; }
+}
