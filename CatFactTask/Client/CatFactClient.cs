2f7c579b9cc9a6cb738f826e9be6f402b4c6b3c2
Jakub Tomaszewski <jakubtomaszewski@vp.pl>
2026-09-07 21:29:08 +0200

    add CatFactClient for requesting facts

---
+++ CatFactTask/Client/CatFactClient.cs
@@ -0,0 +1,13 @@
+﻿using System.Net.Http.Json;
+using CatFactTask.Dto;
+
+namespace CatFactTask.Client;
+
+public class CatFactClient(HttpClient httpClient) : ICatFactClient
+{
+    public async Task<CatFactDto> GetCatFactAsync()
+    {
+        var response = await httpClient.GetFromJsonAsync<CatFactDto>("fact");
+        return response ?? throw new InvalidOperationException("Empty response");
+    }
+}
---
+++ CatFactTask/Client/ICatFactClient.cs
@@ -0,0 +1,8 @@
+﻿using CatFactTask.Dto;
+
+namespace CatFactTask.Client;
+
+public interface ICatFactClient
+{
+    Task<CatFactDto> GetCatFactAsync();
+}
